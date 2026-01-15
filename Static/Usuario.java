f6611e85831833183c91f58199516fe5b02f5c96
felipemacedo6785-ui <felipe.macedo6785@gmail.com>
2026-01-14 23:11:19 -0300

    Usuario.java

--- Static/Usuario.java
+++ Static/Usuario.java
@@ -1,5 +1,12 @@
-package Static;
-
 public class Usuario {
 
+    private static int contador = 0;
+
+    public Usuario() {
+        contador++;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
 }
