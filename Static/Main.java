208bfe08f90d8aee79ea8e32e6323df0c78a800f
felipemacedo6785-ui <felipe.macedo6785@gmail.com>
2026-01-14 23:10:49 -0300

    Main.java

--- Static/Main.java
+++ Static/Main.java
@@ -1,5 +1,10 @@
-package Static;
-
 public class Main {
+    public static void main(String[] args) {
 
+        Usuario u1 = new Usuario();
+        Usuario u2 = new Usuario();
+        Usuario u3 = new Usuario();
+
+        System.out.println(Usuario.getContador());
+    }
 }
